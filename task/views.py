9009bfb460867d03e66ae8029caf8987b302dbb2
viloker <yarmolykvadym@gmail.com>
2026-03-19 21:31:33 +0100

    Add decorator login_required for index function in task/views.py

--- task/views.py
+++ task/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render
 
 
+@login_required
 def index(request):
     return render(request, "task/index.html")
+
