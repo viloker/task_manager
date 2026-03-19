8d6ba41d3b7d721600ddacf85c6e13974a410e26
viloker <yarmolykvadym@gmail.com>
2026-03-19 20:33:50 +0100

    Create TaskAdmin, WorkerAdmin. And register models to admin: TaskType, Position

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Task, TaskType, Worker, Position
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
