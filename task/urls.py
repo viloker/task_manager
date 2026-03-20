75c794c3f1f933efcf7b4f2db9a5ee7ba26e92b4
viloker <yarmolykvadym@gmail.com>
2026-03-20 05:17:31 +0100

    Create paths for Task model

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DetailView,
+    DeleteView
+)
 
 from django.shortcuts import render
 
@@ -7,3 +15,22 @@ from django.shortcuts import render
 def index(request):
     return render(request, "task/index.html")
 
+
+class TaskListView(LoginRequiredMixin, ListView):
+    ...
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    ...
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    ...
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    ...
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    ...
