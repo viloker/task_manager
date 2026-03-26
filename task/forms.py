2b4435f6685ffb2e03fff0271a47e7d95ecb5788
viloker <yarmolykvadym@gmail.com>
2026-03-26 10:59:11 +0100

    Create WorkerForm for create worker

--- task/forms.py
+++ task/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Task
+from .models import Task, Worker
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+class WorkerForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ["username", "password"]
